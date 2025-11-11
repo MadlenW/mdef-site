29f7435d46686a919053681c4c4aba0435d27c34
MadlenW <madlen.elise.von.wulffen@students.iaac.net>
2025-11-11 15:04:33 +0100

    chore: add Tailwind and apply initial styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://madlenw.github.io/mdef-site/',
-  base: '/mdef-site/'
+  base: '/mdef-site/',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
